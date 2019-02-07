f8793476b4bb2aa7e76c6d6c34e785605316f8c9
Appo <appoline.deangelis@gmail.com>
2019-02-07 14:44:32 +0100

    add css + js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert("Hello world");
+// $(window).on("scroll", function(){
+//    let positionY = $(this).scrollTop();
+//    console.log (positionY);
+//    $(window).height();
+// })
+
+// Faire apparaître la flèche quand on arrive en bas de la page
+$(window).on("scroll", function(){
+    if($(this).scrolltotop() > 141){
+        $(".fas").show();
+    }
+
+
+    let positionY = $(this).scrollTop();
+    console.log (positionY);
+})
