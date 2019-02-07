9c140c7a0881787ff098cdcab6a66e8d21a0695e
Appo <appoline.deangelis@gmail.com>
2019-02-07 15:01:18 +0100

    scroll to top js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-// $(window).on("scroll", function(){
-//    let positionY = $(this).scrollTop();
-//    console.log (positionY);
-//    $(window).height();
-// })
-
 // Faire apparaître la flèche quand on arrive en bas de la page
 $(window).on("scroll", function(){
-    if($(this).scrolltotop() > 141){
-        $(".fas").show();
-    }
-
-
     let positionY = $(this).scrollTop();
-    console.log (positionY);
+    if(positionY > 1700){
+        $("#scrolltotop").css("opacity", 1);
+    }
+    if(positionY < 1700){
+        $("#scrolltotop").css("opacity", 0);
+    }
 })
