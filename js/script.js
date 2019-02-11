b765fedbdacb0629a0be13a9f2ef2d148e2982f3
Appo <appoline.deangelis@gmail.com>
2019-02-11 11:11:39 +0100

    navBar en position fixe et ajout de contenu visuel

--- js/script.js
+++ js/script.js
@@ -6,5 +6,29 @@ $(window).on("scroll", function(){
     }
     if(positionY < 1700){
         $("#scrolltotop").css("opacity", 0);
-    }
+    }        
 })
+    
+
+// Positionnement du menu
+$(window).on("scroll", function(){
+    let positionN = $(this).scrollTop();
+    // Menu en position fixe quand on scroll vers le bas
+    if(positionN > 5){
+        $("#navBar").css("position", "fixed");
+        // Propriétés esthétiques du menu
+        $("#navBar").css("width", "100%");
+        $(".menu").css("height", "40px");
+        $(".menu").css("transition", "0.3s");
+        $(".menu").css("opacity", 0.8);
+    }
+    // Position relative du menu quand il est en haut de page
+    if(positionN < 5){
+        $("#navBar").css("position", "relative");
+        // Propriétés esthétiques du menu
+        $("#navBar").css("width", "100%");
+        $(".menu").css("height", "60px");
+        $(".menu").css("transition", "0.3s");
+        $(".menu").css("opacity", 1);
+    }
+})    
